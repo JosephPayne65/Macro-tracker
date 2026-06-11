cc9ba684202cb4ae128ac7177a877ebfe84aa767
Joseph Payne <joepayne622@gmail.com>
2026-06-11 09:05:53 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "macro-tracker-v2";
+const CACHE_NAME = "macro-tracker-v3";
 const ASSETS = ["/", "/index.html"];
 
 self.addEventListener("install", (e) => {
@@ -19,6 +19,12 @@ self.addEventListener("activate", (e) => {
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
